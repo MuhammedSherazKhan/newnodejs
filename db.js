6d01cfaec88ff1b02b9bc0bdc86d69e76fee130d
SHERAZ KHAN <sheraz khan git>
2024-08-03 20:14:12 +0530

    secondversion

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 const mongoose= require('mongoose')
 // define mongodb connection url
-const mongoURL='mongodb://localhost:27017/hotel'
+// const mongoURL='mongodb://localhost:27017/hotel'
+const mongoURL='mongodb+srv://shaazkhan2003:sheraz321@cluster0.i7h7mss.mongodb.net/'
+// mongodb+srv://shaazkhan2003:<password>@cluster0.i7h7mss.mongodb.net/
 // setup connection
 
 mongoose.connect(mongoURL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase the timeout to 30 seconds`
+  socketTimeoutMS: 60000, // Increase socket timeout to 60 seconds
 });
 
 const db=mongoose.connection;
